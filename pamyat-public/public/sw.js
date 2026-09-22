eadf2ac032e3cc9cddcf7347db6189837a7aa9fb
yuvion19 <casketgamer@gmail.com>
2026-09-22 21:12:10 +0300

    Bust PWA cache after map and event form fixes

--- pamyat-public/public/sw.js
+++ pamyat-public/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="pamyat-v2";
+const CACHE="pamyat-v3";
 const STATIC=["/","/manifest.webmanifest","/icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
