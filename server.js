7082bee20c98bdc369928bc9340fb07391d599d6
yuvion19 <casketgamer@gmail.com>
2026-09-25 02:19:21 +0300

    Allow new standalone NeuroHub public domain

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ const corsOrigins = new Set([
   "https://nargila.wixsite.com",
   "https://yuvion.ru",
   "https://www.yuvion.ru",
-  "https://neurohub-zero-yuvion.onrender.com"
+  "https://neurohub-zero-yuvion.onrender.com",
+  "https://neurohub-zero-free-ai.onrender.com"
 ]);
 app.use((req, res, next) => {
   const origin = String(req.get("origin") || "");
