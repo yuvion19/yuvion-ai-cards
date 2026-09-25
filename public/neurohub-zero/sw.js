8b1d23acde17c29af31b6f5398ab58bc8f72322e
yuvion19 <casketgamer@gmail.com>
2026-09-25 03:41:10 +0300

    Update Gideon 6.0 PWA cache

--- public/neurohub-zero/sw.js
+++ public/neurohub-zero/sw.js
@@ -1,4 +1,4 @@
-const CACHE='gideon-os-v4';
+const CACHE='gideon-6-v1';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
